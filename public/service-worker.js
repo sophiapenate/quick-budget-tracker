1e4155a2e5d2b37183b9bd335d7af6034083d60d
Sophia Barrett <sophia@yoursummit.media>
2022-01-22 18:21:49 -0700

    activate service worker and clear old caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = 'budget-tracker-v1';
+const CACHE_NAME = "budget-tracker-v1";
 
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/js/index.js",
-    "/css/styles.css"
-];
+const FILES_TO_CACHE = ["/", "/index.html", "/js/index.js", "/css/styles.css"];
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+// install service worker
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// activate service worker
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+              console.log('Removing old cache data:', key);
+              return caches.delete(key);
+          }
+      }));
+    })
+  );
 });
